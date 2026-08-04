d7fec56aefde8c18fa0de9208b38cd315b433268
wms1125 <2556250259@qq.com>
2026-08-04 23:35:36 +0800

    feat: add engine detection models

---
+++ game_localizer/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    CapabilityLevel,
+    DetectionEvidence,
+    DetectionReport,
+    DetectionResult,
+    DetectionStatus,
+    MaturityLevel,
+)
+
+__all__ = [
+    "CapabilityLevel",
+    "DetectionEvidence",
+    "DetectionReport",
+    "DetectionResult",
+    "DetectionStatus",
+    "MaturityLevel",
+]
